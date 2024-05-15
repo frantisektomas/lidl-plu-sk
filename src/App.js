cf79490dc5b1cb6757713acb560e7b5a491258c2
frantisektomas <152633710+frantisektomas@users.noreply.github.com>
2024-05-15 17:51:52 +0200

    adding products

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { Routes, Route } from 'react-router';
+import Home from './home/home';
 
 function App() {
   return (
-    <>
-
-    </>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+    </Routes>
   );
 }
 
